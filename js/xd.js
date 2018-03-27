717aeb9c333d6680504023e576f51a2ecbe2e2d9
Adam <cabridgepl@wp.pl>
2018-03-28 00:22:58 +0200

    cwiczonko

---
+++ js/xd.js
@@ -0,0 +1,14 @@
+var jaj1 = document.getElementById("jajco1");
+var jaj2 = document.getElementById("jajco2");
+var jaj3 = document.getElementById("jajco3");
+var jaj4 = document.getElementById=("jajco4");
+var okienko = document.getElementById=("okno");
+
+jaj1.innerHTML = 'gówno w dupie';
+jaj2.innerHTML = 'smacznie chlupie';
+jaj3.innerHTML = 'tak jest';
+jaj4.innerHTML = 'nie';
+
+
+var okienko1 = prompt('podaj cos');
+jaj1.innerHTML = okienko1;
